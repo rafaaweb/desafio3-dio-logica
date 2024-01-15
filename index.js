6f7b8b2cb5e80d6ebe7345570b3b168b0c3b554c
rafaaweb <rafa.ads@outlook.com>
2024-01-15 20:57:20 -0300

    Desafio 3 DIO

---
+++ index.js
@@ -0,0 +1,43 @@
+// Definindo a classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'realizou um ataque';
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Criando instâncias da classe Heroi
+const heroi1 = new Heroi('Aragorn', 30, 'guerreiro');
+const heroi2 = new Heroi('Gandalf', 1000, 'mago');
+const heroi3 = new Heroi('Bruce Lee', 32, 'monge');
+const heroi4 = new Heroi('Hattori Hanzo', 25, 'ninja');
+
+// Chamando o método atacar para cada herói
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
